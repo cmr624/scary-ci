fc1d24dc568fea75106d2f5a4ec8d3e8e72aed67
Paul Barbato <pauljamesbarbato@gmail.com>
2023-10-07 21:31:11 -0400

    this is the cutscene tool
    +cutscene tool

---
+++ Assets/Image Sequence Playback/Scripts/Examples/materialMode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class materialMode : MonoBehaviour 
+{
+    public Playback playback;
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 30), "Play"))
+        {
+            playback.Play();
+        }
+        if (GUI.Button(new Rect(220, 10, 100, 30), "Pause"))
+        {
+            playback.Pause(!playback.IsPaused);
+        }
+        playback.loop = GUI.Toggle(new Rect(10, 50, 100, 30), playback.loop, "Loop");
+    }
+}
---
+++ Assets/Image Sequence Playback/Scripts/Examples/uGUI_Example_UI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class uGUI_Example_UI : MonoBehaviour 
+{
+    public Playback p;
+
+    public void OnPlayButtonClick()
+    {
+        p.Play();
+    }
+
+    public void OnPauseButtonClick()
+    {
+        p.Pause(!p.IsPaused);
+    }
+
+    public void OnStopButtonClick()
+    {
+        p.Stop();
+    }
+}
